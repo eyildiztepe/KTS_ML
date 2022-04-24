9723cafb33988c677eccec7b103e4de9a7dea72b
yumbalina <gorener.cem@gmail.com>
2022-04-25 00:41:59 +0300

    Chapter3 and Neural Networks

--- deneme.R
+++
@@ -1,9 +0,0 @@
-setwd("C:/Users/Cem/Desktop/BP_Proje/KTS_ML")
-library(tidyverse)
-CTS <- as.data.frame(read_csv("data/CTS.csv"))
-seed<-0923
-set.seed(seed)
-ind<-sample(2,nrow(CTS),replace = T,prob = c(0.7,0.3))
-traindata_top <- CTS[ind==1,]
-testdata_top <- CTS[ind==2,]
-write.table(x, file, append = FALSE, sep = ",", dec = ".",row.names = FALSE, col.names = TRUE)
